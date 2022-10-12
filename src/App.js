50658008709c5c71b67ea6b95f7b4088ae07a764
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-10-12 13:03:27 +0300

    5

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.scss";
 
 import React from 'react'
 import Navbar from "./coponents/Navbar/Navbar";
+import Header from "./coponents/Header/Header";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar/>
+      <Header/>
     </div>
   )
 }
---
+++ src/coponents/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+const Header = () => {
+    return(
+        <div>
+            <h1>Popular Tour Place</h1>
+        </div>
+    )
+}
+
+export default Header;
